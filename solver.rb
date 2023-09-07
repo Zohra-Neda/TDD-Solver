4d3bfab80c34abff3f1ea172ea63bf7417afaf50
zohra-neda <zohra.neda@gmail.com>
2023-09-07 13:45:59 +0430

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,26 +1,28 @@
 class Solver
-    def factorial(n)
-        if n<0
-            raise ArgumentError, "No factorial for negative numbers"
-        elsif n == 0
-            return 1
-        else
-            result = 1
-            (1..n).each do |i|
-                result *= i
-            end
-        end
-        return result
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'No factorial for negative numbers'
+    elsif num.zero?
+      return 1
+    else
+      result = 1
+      (1..num).each do |i|
+        result *= i
+      end
     end
 
-    def reverse(word)
-        return word.split('').reverse.join('')
-    end
+    result
+  end
 
-    def fizzbuzz(number)
-        return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
-        return 'fizz' if (number % 3).zero?
-        return 'buzz' if (number % 5).zero?
-        number.to_s
-    end
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
