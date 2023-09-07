c7fe23e267de0f8dfbf93bd67fef4650fd26bc78
Cosmas Wasswa <cosywas@gmail.com>
2023-09-07 11:21:16 +0300

    Add test for number being 5

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,5 +11,10 @@ describe Solver do
         solve = Solver.new
         expect(solve.factorial(0)).to eq(1)
     end
+
+    it 'should return 120 if number is 5' do
+        solve = Solver.new
+        expect(solve.factorial(5)).to eq(120)
+    end
 end
 end
