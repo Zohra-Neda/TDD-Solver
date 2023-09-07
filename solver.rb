d8be5bc7ad48cd14a7ee38cd09c8b69b7a37183c
zohra-neda <zohra.neda@gmail.com>
2023-09-07 13:38:11 +0430

    Write a method to return number as string  if the number is not  divisible by 3 and  5

--- solver.rb
+++ solver.rb
@@ -21,5 +21,6 @@ class Solver
         return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
         return 'fizz' if (number % 3).zero?
         return 'buzz' if (number % 5).zero?
+        number.to_s
     end
 end
