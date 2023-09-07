24174fa60675775e5335f76ddc341ac84d7d2694
Cosmas Wasswa <cosywas@gmail.com>
2023-09-07 11:26:10 +0300

    Implement factorial method for numbers greater than 0

--- solver.rb
+++ solver.rb
@@ -4,6 +4,12 @@ class Solver
             raise ArgumentError, "No factorial for negative numbers"
         elsif n == 0
             return 1
+        else
+            result = 1
+            (1..n).each do |i|
+                result *= i
+            end
         end
+        return result
     end
 end
