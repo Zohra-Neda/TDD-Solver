a7f0a5f6126e21664a55e9817f8387d22ec3f3d7
Cosmas Wasswa <cosywas@gmail.com>
2023-09-07 11:18:07 +0300

    Implement factorial when number is 0

--- solver.rb
+++ solver.rb
@@ -2,6 +2,8 @@ class Solver
     def factorial(n)
         if n<0
             raise ArgumentError, "No factorial for negative numbers"
+        elsif n == 0
+            return 1
         end
     end
 end
