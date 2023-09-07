c3eb07da072fa7489d031d684db6b2821270191e
Cosmas Wasswa <cosywas@gmail.com>
2023-09-07 11:15:18 +0300

    Test for factorial of 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,5 +6,10 @@ describe Solver do
         solve = Solver.new
         expect{solve.factorial(-2)}.to raise_error(ArgumentError)
     end
+
+    it 'should return 1 when number is 0' do
+        solve = Solver.new
+        expect(solve.factorial(0)).to eq(1)
+    end
 end
 end
